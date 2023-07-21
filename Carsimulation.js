80b703ec415bcaea48da9ad1a61f579fb91267a9
Muhammad Haaris <muhammadhaaris.j@gmail.com>
2023-07-20 20:01:13 -0400

    Added class for Car and Constructor

---
+++ Carsimulation.js
@@ -0,0 +1,10 @@
+class Car {
+    constructor(brand, model, year, color, price, gas) {
+      this.brand = brand;
+      this.model = model;
+      this.year = year;
+      this.color = color;
+      this.price = price;
+      this.gas = gas;
+    }
+}
