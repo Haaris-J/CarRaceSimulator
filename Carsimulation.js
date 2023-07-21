d29144d72788f1279f2e3d11404d7a7accf01696
Muhammad Haaris <muhammadhaaris.j@gmail.com>
2023-07-20 20:02:41 -0400

    Added data to Class

--- Carsimulation.js
+++ Carsimulation.js
@@ -8,3 +8,13 @@ class Car {
       this.gas = gas;
     }
 }
+
+// Create car objects
+const cars = [
+    new Car("Honda", "CR-V", 2023, "Red", 50000, 45),
+    new Car("Ford", "F-150", 2020, "Black", 25000, 30),
+    new Car("BMW", "X5", 2022, "Green", 60000, 65),
+    new Car("Mazda", "CX-5", 2019, "White", 15000, 60),
+    new Car("Audi", "Q7", 2018, "Silver", 52000, 47),
+    new Car("Kia", "Forte", 2020, "Blue", 21000, 56),
+  ];
